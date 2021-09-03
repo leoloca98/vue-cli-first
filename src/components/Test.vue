aab7791f24d433d463107f14641f1f3d314f39be
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-03 17:06:30 +0200

    Create Project + Add New Components

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="add-components">
+    <h1>Aggiunta di un'altro componente</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.add-components h1 {
+  color: forestgreen;
+  line-height: 10px;
+  text-decoration: dashed;
+}
+</style>
